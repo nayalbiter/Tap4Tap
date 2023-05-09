d799929e30ffd87e9279399ad5bb5193dcbefec5
carolsol23 <s-carolina.solar-mor@lwtech.edu>
2023-05-09 16:12:08 -0700

    Create minimal brewery POJO

---
+++ src/main/java/edu/lwtech/csd297/tap4tap/pojos/Brewery.java
@@ -0,0 +1,94 @@
+package edu.lwtech.csd297.tap4tap.pojos;
+
+public class Brewery {
+    private String breweryId;
+    private String name;
+    private String breweryType;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String city;
+    private String stateProvince;
+    private String postalCode;
+    private String country;
+    private String websiteUrl;
+    private String phone;
+    private String longitude;
+    private String latitude;
+
+    public Brewery(String breweryId, String name, String breweryType,
+            String address1, String address2, String address3, String city,
+            String stateProvince, String postalCode, String country,
+            String websiteUrl, String phone, String longitude, String latitude ){
+                this.breweryId = breweryId;
+                this.name = name;
+                this.breweryType = breweryType;
+                this.address1 = address1;
+                this.address2 = address2;
+                this.address3 = address3;
+                this.city = city;
+                this.stateProvince = stateProvince;
+                this.postalCode = postalCode;
+                this.country = country;
+                this.websiteUrl = websiteUrl;
+                this.phone = phone;
+                this.longitude = longitude;
+                this.latitude = latitude;
+    }
+
+    public String getBreweryId() {
+        return breweryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreweryType() {
+        return breweryType;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getAddress3() {
+        return address3;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+}
