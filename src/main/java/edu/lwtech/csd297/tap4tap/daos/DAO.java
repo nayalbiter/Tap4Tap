02aea70ad0d6fd22f682c1091cc1c526f2d5ce30
carolsol23 <s-carolina.solar-mor@lwtech.edu>
2023-05-11 16:00:19 -0700

    Last changes

--- src/main/java/edu/lwtech/csd297/tap4tap/daos/DAO.java
+++ src/main/java/edu/lwtech/csd297/tap4tap/daos/DAO.java
@@ -6,7 +6,7 @@ import edu.lwtech.csd297.tap4tap.pojos.SearchParameter;
 
 // Generic Data Access Object (DAO) Interface
 
-public interface DAO<T, K> {
+public interface DAO<T> {
 
     // Life Cycle ----------------------------------------
     boolean initialize(String initParams);
